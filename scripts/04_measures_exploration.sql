80719b81ddbbaba08668d832dbc2a74c06730a8d
Marsel Pongdatu Luase <luaseachell@gmail.com>
2025-12-01 17:29:28 +0800

    Create 04_measures_exploration.sql

---
+++ scripts/04_measures_exploration.sql
@@ -0,0 +1,49 @@
+/*
+==============================================================================================
+Measures Exploration (Key Metrics)
+==============================================================================================
+Purpose:
+    - To calculate the key metric of the business (e.g., totals, averages) for quick insights.
+    - To identify overall trends or spot anomalies.
+
+SQL Functions Used:
+    - COUNT(), SUM(), AVG()
+==============================================================================================
+*/
+
+-- Find the Total Sales
+SELECT  SUM(sales_amount) AS total_sales FROM gold.fact_sales;
+
+-- Find how many items are sold
+SELECT  SUM(quantity) AS total_quantity FROM gold.fact_sales;
+
+-- Find the average  selling price
+SELECT AVG(price) AS avg_price FROM gold.fact_sales;
+
+-- Find the Total Number of Orders
+SELECT COUNT(order_number) AS total_orders FROM gold.fact_sales;
+SELECT COUNT(DISTINCT order_number) AS total_orders FROM gold.fact_sales;
+
+-- Find the Total Number of products
+SELECT COUNT(product_name) AS total_products FROM gold.dim_products;
+
+-- Find the Total Number of Customers
+SELECT COUNT(customer_key) AS total_number_customer FROM gold.dim_customers;
+
+-- Find the Total Number of Customer that has placed an order
+SELECT COUNT(DISTINCT customer_key) AS total_customer FROM gold.fact_sales;
+
+
+--Generate a Report that shows all key metrics of the business
+
+SELECT 'Total Sales' as measure_name, SUM(sales_amount) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Quantity', SUM(quantity) FROM gold.fact_sales
+UNION ALL
+SELECT 'Average Price', AVG(price) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Nr. Orders', COUNT(DISTINCT order_number) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Nr. Products', COUNT(product_name) FROM gold.dim_products
+UNION ALL
+SELECT 'Total Nr. Customers', COUNT(customer_key) FROM gold.dim_customers;
